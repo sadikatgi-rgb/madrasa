ae7958b43bc2719b1559babcd2c39aef55f5f282
sadikatgi-rgb <sadikatgi@gmail.com>
2026-05-01 08:20:43 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+// Scripts for firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// 1. Firebase Configuration (നിങ്ങൾ നൽകിയത്)
+const firebaseConfig = {
+    apiKey: "AIzaSyDgWlTlsbkqIo2xQFf-UI6fE6X1KeyI9-U",
+    authDomain: "madrasa-acf90.firebaseapp.com",
+    projectId: "madrasa-acf90",
+    storageBucket: "madrasa-acf90.firebasestorage.app",
+    messagingSenderId: "128246256646",
+    appId: "1:128246256646:web:49e41f5f8d16340550df87",
+    measurementId: "G-3FHMCRGX0W"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+// Background messaging handler
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/log.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
